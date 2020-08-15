dbbc225aa3a6b2687ace9d235e081f75947ebd65
zkcheng0827 <69299540+zkcheng0827@users.noreply.github.com>
2020-08-15 10:11:25 +0100

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -20,4 +20,21 @@ public:
                 missing.push_back(i+1);
         return missing;
     }
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+    /*
+    448. Find All Numbers Disappeared in an Array
+    64%
+    */
+        for (const auto& n: nums) {
+            auto idx = abs(n) - 1;
+            if (nums[idx] > 0) {
+                nums[idx] = -nums[idx];
+            }
+        }
+        vector<int> out;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) out.push_back(i+1);
+        }
+        return out;
+    }
 };
